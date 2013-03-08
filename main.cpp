dfc0f7d2929e3767447ae91b08205082da49cf1d
Isaias Lourenco <isaias.lourenco@swp.pt>
2013-03-08 14:15:19 +0000

    Added a help and embeded the filename to the data

--- main.cpp
+++ main.cpp
@@ -60,6 +60,7 @@ void compress(string fn)
 
     string filecompressed = "";
 
+    //Put the filename into the begining of the data
     input = fn;
 
     ifstream in(fn.c_str(), ios::in | ios::binary);
@@ -71,15 +72,18 @@ void compress(string fn)
         in.seekg(0, ios::beg);
         in.read(&contents[0], contents.size());
         in.close();
+        //Append the uncompressed data to the variable containing the file name
         input.append(contents);
     }
     else
         exit(1);
 
+    //Compress the stream containing the filename and the data
     snappy::Compress(input.data(), input.size(), &output);
 
     filecompressed.append(filename).append(".").append(FILEEXTENSION);
 
+    //Save the compressed data to the disk
     ofstream f(filecompressed.c_str(), ios::binary | ios::out);
     f << output;
 }
@@ -104,15 +108,23 @@ void decompress(string fn)
 
     snappy::Uncompress(input.data(), input.size(), &output);
 
+    //Remove the filename from the uncompressed data
     fileoriginal = output.substr(0, output.find(".") + 4);
     output.erase(0, fileoriginal.size());
 
+    //Save the uncompressed data to disk
     ofstream f2(fileoriginal.c_str(), ios::binary | ios::out);
     f2 << output;
 }
 
 void showHelp()
 {
-    printf("Snappy help... TODO:\n");
+    printf("Snappy Test 1.0\n\n");
+    printf("Usage: snappytest <options> <filename>\n\n");
+    printf("Options:\n");
+    printf("     -c  Compress a file\n");
+    printf("     -x  Extract a file\n\n");
+    printf("Example:\n");
+    printf("  snappytest -c logo.bmp\n");
     exit(0);
 }
